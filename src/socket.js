7a3d81e593fe249b15f8f8281943b72a7eb14b1e
aslam0033 <aslammujawar00033@gmail.com>
2025-11-01 19:29:03 +0530

    some errors handled

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 
-const BACKEND = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
+const BACKEND = import.meta.env.VITE_BACKEND_URL || 'https://dbms-ka-sikandar-f.vercel.app/';
 export const socket = io(BACKEND, { autoConnect: false });
 
 export default socket;
